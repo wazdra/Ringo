88bdd5720a20214efa806d0ca3e71d34f3b7f4ff
Marie <mmarie.laporte@gmail.com>
2016-05-27 12:21:42 +0200

    fdjsh

--- src/Message.java
+++ src/Message.java
@@ -1,10 +1,23 @@
+import java.util.*;
+
+
 public class Message extends Application{
+    public final int maxLength = 485;
+
     public Message(Entity e){
         this.entity = e;
         this.id = "DIFF####";
     }
     public static int getSizeMess(String msg){
         return Integer.parseInt(msg.substring(0,3));
+    }
+
+    public static String lengthToNW(int length){
+        String str = "" + length;
+        while(str.length() < 3){
+            str = "0" + str;
+        }
+        return str;
     }
 
     public static String getMess(String msg, int size){
@@ -14,5 +27,24 @@ public class Message extends Application{
     public void handleApp(String msg){
         Invite.addMsg("Message reçu : "+getMess(msg,getSizeMess(msg))); // Ajout du message à transférer dans l'invite de commande
         entity.sendUDP(msg);
+    }
+
+    public void useApp(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Pour sortir du mode interactif, appuyez sur ENTREE.");
+        System.out.print("Ecrivez votre message : ");
+        String msg="";
+        do{
+            if (msg.length() > maxLength) System.out.print("Message trop long ! \n Ecrivez votre message : ");
+            msg = sc.nextLine();
+        } while(msg.length() > maxLength);
+
+        if (msg.length() > 0){
+            entity.sendUDP(entity.getAppRequest(this.id, "" + lengthToNW(msg.length()) + " " + msg));
+        }
+    }
+
+    public void useApp(String msg){
+
     }
 }
